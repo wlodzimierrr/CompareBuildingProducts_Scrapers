62dba452e4e784dc0bd8861e7fb40737a172884c
wlodzimierrr <szerszywlodzimierz@gmail.com>
2024-07-30 13:07:39 +0100

    update shop_id in b&q scraper

--- scraper_factory/src/BandQ/bandq.py
+++ scraper_factory/src/BandQ/bandq.py
@@ -26,7 +26,7 @@ def get_scraping_target_data():
         cursor.close()
         conn.close()
         
-        return [{"shop_id": 2, "category_code": row[0], "page_url": row[1], "category": row[2], "subcategory": row[3]} for row in data]
+        return [{"shop_id": 1, "category_code": row[0], "page_url": row[1], "category": row[2], "subcategory": row[3]} for row in data]
     except Exception as e:
         logging.error(f"Error connecting to the database: {e}")
         raise e    
